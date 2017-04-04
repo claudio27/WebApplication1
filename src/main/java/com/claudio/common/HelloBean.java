2080a9b618be663083de97f4843fb7a0bcafb464
Claudio Serrano <letresorinterdit@gmail.com>
2017-04-04 15:36:19 -0300

    Probando envio multiple a procesamiento con ajax

--- src/main/java/com/claudio/common/HelloBean.java
+++ src/main/java/com/claudio/common/HelloBean.java
@@ -8,10 +8,11 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class HelloBean implements Serializable {
-	
-	
+
 	private static final long serialVersionUID = 1L;
 	private String name;
+	private String name2;
+	private String name3;
 
 	public String getName() {
 		return name;
@@ -20,15 +21,30 @@ public class HelloBean implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public String getName2() {
+		return name2;
+	}
+
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+
+	public String getName3() {
+		return name3;
+	}
+
+	public void setName3(String name3) {
+		this.name3 = name3;
+	}
+
 	public String getSayWelcome() {
 		// check if null?
 		if ("".equals(name) || name == null) {
 			return "";
 		} else {
-			return "Ajax message : Welcome " + name;
+			return "Ajax message : Welcome " + name + ", " + name2 + ", " + name3;
 		}
 	}
-	
 
 }
